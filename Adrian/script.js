b781a7d23f9dd5688a732db9a3bba922d9a421ec
Unknown <AAlvarado@student.neumont.edu>
2018-05-25 09:21:04 -0600

    Update with my stuff in Adrian
    Contains basic containers and styling for index, and some commented script code. Has some features for mobile styling.

---
+++ Adrian/script.js
@@ -0,0 +1,9 @@
+//Example from in class
+// var btnpep = document.getElementById('btn-pepperoni');
+// var pizza = document.getElementById('pizza');
+
+// btnpep.addEventListener('click', addPepperoni);
+
+// function addPepperoni(evt) {
+//   pizza.style.background = "url(pepperoni.png), url(blackolives.png), url(mushrooms.png), url(pizza.png)";
+// }
